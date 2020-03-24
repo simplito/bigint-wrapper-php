6ea50941e5e31241eee573f9fdde7366f75955d3
Ruslan Gataullin <fisher7410@yandex.ru>
2020-03-24 13:55:52 +0500

    Added shiftLeft method based on https://docs.microsoft.com/ru-ru/dotnet/api/system.numerics.biginteger.op_leftshift

--- lib/BigInteger.php
+++ lib/BigInteger.php
@@ -625,6 +625,10 @@ class BigInteger{
     public function sign() {
         return $this->value[0] === "-" ? -1 : ($this->value === "0" ? 0 : 1);
     }
+
+    public function shiftLeft($n) {
+        return $this->mul((new static(2))->pow($n));
+    }
 }
 
 }
